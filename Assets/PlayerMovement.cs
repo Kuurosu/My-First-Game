c9c9e2186641bb4825e65b33a7c2da850c95c275
Rob Sizeland <robcakes69@gmail.com>
2023-01-14 21:29:31 +0000

    Initial commit

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,10 +5,12 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody rb;
 
+    public float forwardForce = 800f;
+
     // We marked this with "Fixed"Update because
     // we are using it to mess with physics
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 200 * Time.deltaTime); // add a force of 200 on the z-axis
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // add a force of 200 on the z-axis
     }
 }
