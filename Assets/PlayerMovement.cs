a2f54f51c155f0752b08e3db4c8d96d08dd7fc28
Rob Sizeland <robcakes69@gmail.com>
2023-01-14 21:56:02 +0000

    Add player movement and following camera

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,12 +5,32 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody rb;
 
-    public float forwardForce = 800f;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 800f;
 
     // We marked this with "Fixed"Update because
     // we are using it to mess with physics
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // add a force of 200 on the z-axis
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
     }
 }
